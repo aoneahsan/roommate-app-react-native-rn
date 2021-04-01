4cf5fcca26989aece54d130c039ef893fd14bdb9
ahsanmahmood786 <ahsan.mahmood@shopdev.co>
2021-04-01 23:05:56 +0500

    fix: updated

--- src/screens/LandingScreen/index.js
+++ src/screens/LandingScreen/index.js
@@ -1,16 +1,44 @@
 // Core Imports
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, ImageBackground } from "react-native";
+
+// Custom Imports
+import LandingScreenBgImage from "./../../../assets/images/main-screen-bg.png";
+import Logo from "./../../components/Logo";
+import CustomButton from "./../../components/CustomButton";
 
 class LandingScreen extends React.Component {
-  state = {};
-
-  componentDidMount() {}
-
   render() {
     return (
       <View style={STYLES.main}>
-        <Text>working</Text>
+        <ImageBackground source={LandingScreenBgImage} style={STYLES.mainBgCon}>
+          <View style={STYLES.contentCon}>
+            <View style={STYLES.contentInnerCon}>
+              <Logo />
+            </View>
+            <View style={STYLES.contentInnerCon}>
+              <View style={STYLES.btnsCon}>
+                <View style={STYLES.btnCon}>
+                  <CustomButton
+                    style={STYLES.btn}
+                    color="white"
+                    onPress={() => alert("Sign Up")}
+                  >
+                    Sign Up
+                  </CustomButton>
+                </View>
+                <View style={STYLES.btnCon}>
+                  <CustomButton
+                    style={STYLES.btn}
+                    onPress={() => alert("Log In")}
+                  >
+                    Log In
+                  </CustomButton>
+                </View>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
       </View>
     );
   }
@@ -20,6 +48,25 @@ const STYLES = StyleSheet.create({
   main: {
     flex: 1,
   },
+  mainBgCon: {
+    width: "100%",
+    height: "100%",
+  },
+  contentCon: {
+    flex: 1,
+  },
+  contentInnerCon: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  btnsCon: {
+    alignItems: "center",
+  },
+  btnCon: {
+    width: "85%",
+    marginVertical: 8,
+  },
+  btn: {},
 });
 
 export default LandingScreen;
