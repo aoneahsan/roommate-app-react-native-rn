8951ceb3f39348a5c3427897c252857cf895de5f
Ahsan Mahmood <ahsan@perkforce.com>
2021-08-05 08:13:35 +0500

    fix: updated

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,4 @@
 // exports.API_ROOT_URL = "http://52.221.206.226/api/v1"; // production URL
-exports.API_ROOT_URL = "http://5951e341f885.ngrok.io/api/v1"; // local URL
+exports.API_ROOT_URL = "http://49cb847e2e5a.ngrok.io/api/v1"; // local URL
 exports.LOCAL_AUTH_KEY = "LOCAL_AUTH_KEY";
 exports.AXIOS_HEADER_AUTH_KEY = "X-Authorization";
