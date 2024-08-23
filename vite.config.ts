c0c21c8e7ee2ef5761679ce37e3f8ff554567045
Ahsan Mahmood <aoneahsan@gmail.com>
2024-08-23 18:49:49 +0500

    packages updated, and useless one removed

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,6 @@ const __dirname = path.dirname(_filePath);
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), legacy()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './src/setupTests.ts',
-	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
